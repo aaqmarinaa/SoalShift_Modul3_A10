1f47c1f81214df074eb7c0ffd877d3c574a96906
aaqmarinaa <almasaqmarina@gmail.com>
2017-10-24 22:46:35 +0700

    nambah nomer 4

--- Soal4.c
+++ Soal4.c
@@ -1,3 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
+
+pthread_t faktor;
+
+void *faktorial(void *ptr){
+}
+
+int main(){
+
+   pthread_create(&faktor, NULL, &faktorial, NULL);
+   pthread_join(faktor, NULL);
+}
