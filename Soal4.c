cbe3e4d8c9e5e5e605689fa5f84d09e6e31273a5
aaqmarinaa <almasaqmarina@gmail.com>
2017-10-24 22:59:29 +0700

    + nmr 4

--- Soal4.c
+++ Soal4.c
@@ -4,11 +4,20 @@
 
 pthread_t faktor;
 
-void *faktorial(void *ptr){
+void faktorial(){
+   int i, bil;
+   for(i=1; i<=bil; i++)
+   {
+	faktor=i*faktor;
+   }
 }
 
 int main(){
-
+   int faktor=1, bil;
+   printf("./faktorial ");
+   scanf("%d ", &bil);
+   faktorial();
+   printf("Hasil %d! = %d\n", bil, faktor);
    pthread_create(&faktor, NULL, &faktorial, NULL);
    pthread_join(faktor, NULL);
 }
