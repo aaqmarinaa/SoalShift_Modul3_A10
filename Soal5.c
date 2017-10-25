bbf441028362a67a977fa1a5f80653dc0f7b2073
aaqmarinaa <almasaqmarina@gmail.com>
2017-10-25 16:30:55 +0700

    update soal5

--- Soal5.c
+++ Soal5.c
@@ -4,7 +4,28 @@
 #include <pthread.h>
 #include <unistd.h>
 
-pthread_t 
-int main(){
-   pthread_create(
+void *baca(void *arg){
+   FILE *fp;
+   fp=fopen("Novel.txt", "r+");
+   int hitung;
+   char word[500];
+   char Novel[999];
+   while(fscanf(fp, "%s\n", Novel) != EOF){
+      if(strstr(Novel, word) != NULL)
+         hitung++;
+   }
+   printf("%d\n", hitung);
+}
+int main(int argc, char* argv[]){
+   pthread_t novel[argc];
+   printf("./cari ");
+   scanf("%s", &word[]);
+   int i, temp;
+   for(i=1; i<=argc; i++){
+	temp = atoi(argv[i]);
+	pthread_create(&(novel[i]), NULL, &baca, (void*)temp);
+   }
+   for(i=1; i<=argc; i++){
+	pthread_join(novel[i], NULL);
+   }
 }
